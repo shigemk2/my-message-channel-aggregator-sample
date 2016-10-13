9238ec154181fe9cdb49b75c4df542df6163baa0
shigemk2 <i.am.shige@gmail.com>
2016-10-13 21:32:34 +0900

    Add case classes

--- src/main/scala/com/example/Aggregator.scala
+++ src/main/scala/com/example/Aggregator.scala
@@ -3,6 +3,24 @@ package com.example
 import akka.actor._
 import com.example._
 
+case class RequestForQuotation(rfqId: String, retailItems: Seq[RetailItem]) {
+  val totalRetailPrice: Double = retailItems.map(retailItem => retailItem.retailPrice).sum
+}
+
+case class RetailItem(itemId: String, retailPrice: Double)
+
+case class PriceQuoteInterest(quoteId: String, quoteProcessor: ActorRef, lowTotalRetail: Double, highTotalRetail: Double)
+
+case class RequestPriceQuote(rfqId: String, itemId: String, retailPrice: Double, orderTotalRetailPrice: Double)
+
+case class PriceQuote(quoterId: String, rfqId: String, itemId: String, retailPrice: Double, discountPrice: Double)
+
+case class PriceQuoteFulfilled(priceQuote: PriceQuote)
+
+case class RequiredPriceQuotesForFulfillment(rfqId: String, quotesRequested: Int)
+
+case class QuotationFulfillment(rfqId: String, quotesRequested: Int, priceQuotes: Seq[PriceQuote], requester: ActorRef)
+
 object AggregatorDriver {
   def main(args: Array[String]): Unit = {
   }
